caad10ac62c34c402cb0a5fdfa9a0d2f72eca25f
mps9506 <mpschramm@gmail.com>
2021-09-07 13:00:39 -0500

    logo function

---
+++ R/add_TWRI_logo.R
@@ -0,0 +1,31 @@
+#' Add logo to ggplot
+#'
+#' @param plot ggplot object
+#' @param scale scale the logo, defaults to 0.25
+#' @param halign aligns logo within the ggplot [0-1]
+#' @param valign aligns the logo within the ggplot [0-1]
+#' @importFrom cowplot ggdraw draw_plot draw_image
+#' @importFrom fs path_package
+#' @return ggplot object
+#' @export
+#'
+add_TWRI_logo <- function(plot,
+                          scale = 0.25,
+                          halign = 0.01,
+                          valign = 0.01) {
+
+
+  logo_path <- fs::path_package("twriTemplates", "rmarkdown", "templates", "twri-docx", "skeleton", "twri_logo.png")
+
+  cowplot::ggdraw() +
+    cowplot::draw_plot(plot) +
+    cowplot::draw_image(logo_path,
+                        x = 0,
+                        y = 0,
+                        scale = scale,
+                        hjust = 0,
+                        vjust = 0,
+                        halign = halign,
+                        valign = valign)
+
+}
--- R/onload.R
+++ R/onload.R
@@ -1,9 +1,14 @@
-.onLoad <- function(libname, pkgname){
-
+.onAttach <- function(...) {
   ## Register desired system fonts
 
   message("registering fonts...")
   fonts <- systemfonts::system_fonts()
 
   try(init_fonts(fonts), outFile = "Could not register required fonts, please download and install from: https://agrilife.tamu.edu/wp-content/uploads/2021/03/AgriFonts.zip")
+}
+
+
+.onLoad <- function(...){
+  cowplot::set_null_device("agg")
+
 }
