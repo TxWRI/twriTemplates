22b716f059ecf737ce4e1406c734ab242c355224
mps9506 <mpschramm@gmail.com>
2021-09-07 15:40:13 -0500

    fix tests to include format functions remove rendered stuff when manually doing tests cleanup remaining files

--- tests/testthat/test-template.R
+++ tests/testthat/test-template.R
@@ -20,3 +20,14 @@ test_that("twri-docx", {
 test_that("twri-pdf", {
   expect_knit("twri-pdf")
 })
+
+
+
+
+test_that("twri-docx returns rmarkdown class", {
+  expect_s3_class(twri_docx(), "rmarkdown_output_format")
+})
+
+test_that("twri-pdf returns rmarkdown class", {
+  expect_s3_class(twri_pdf(), "rmarkdown_output_format")
+})
