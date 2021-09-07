8d55ad703a292210e5cf82576a773dee3c5f5364
mps9506 <mpschramm@gmail.com>
2021-09-07 14:20:31 -0500

    template tests

---
+++ tests/testthat/test-template.R
@@ -0,0 +1,22 @@
+
+skip_on_cran()
+skip_on_ci()
+
+expect_knit <- function(template){
+  expect_output(
+    expect_message(
+      rmarkdown::render(
+        system.file("rmarkdown", "templates", template, "skeleton", "skeleton.Rmd", package = "twriTemplates"),
+        output_file = sprintf("%s-test", template),
+        output_dir = file.path(getwd(), "rendered")),
+      "Output created"),
+    "pandoc")
+}
+
+test_that("twri-docx", {
+  expect_knit("twri-docx")
+})
+
+test_that("twri-pdf", {
+  expect_knit("twri-pdf")
+})
