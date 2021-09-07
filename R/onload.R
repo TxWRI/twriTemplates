5ddb5e1bd9cb81faea56904422f04b054f549b5e
mps9506 <mpschramm@gmail.com>
2021-09-07 14:07:50 -0500

    fix startup messaging

--- R/onload.R
+++ R/onload.R
@@ -1,16 +1,17 @@
 .onAttach <- function(...) {
-  ## Register desired system fonts
 
-  message("registering fonts...")
-  fonts <- systemfonts::system_fonts()
-
-  try(init_fonts(fonts), outFile = "Could not register required fonts, please download and install from: https://agrilife.tamu.edu/wp-content/uploads/2021/03/AgriFonts.zip")
-
-  message("See https://github.com/TxWRI/twriTemplates for more details, use the Issues tab if you have questions.")
+  packageStartupMessage("See https://github.com/TxWRI/twriTemplates for more details, use the Issues tab if you have questions.")
 }
 
 
 .onLoad <- function(...){
+  ## Use agg here to prevent error messages in add_TWRI_logo
+  ## in general, encourage users to utilize ragg package to
+  ## maximize export quality.
   cowplot::set_null_device("agg")
+
+  ## Register desired system fonts
+  fonts <- systemfonts::system_fonts()
+  try(init_fonts(fonts), outFile = "Could not register required [Moriston] fonts, please download and install from: https://agrilife.tamu.edu/wp-content/uploads/2021/03/AgriFonts.zip")
 
 }
