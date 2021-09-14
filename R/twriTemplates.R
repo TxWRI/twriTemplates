39929cfcf83ed60d82fb2124ee5d406b177c68a1
mps9506 <mpschramm@gmail.com>
2021-09-14 13:29:07 -0500

    package documentation

---
+++ R/twriTemplates.R
@@ -0,0 +1,4 @@
+#' @description
+#' The twriTemplates package provides rmarkdown templates that follow internal brand guidance. It also provides functions for formatting ggplot2 plots that follow brand guides.
+#'
+"_PACKAGE"
