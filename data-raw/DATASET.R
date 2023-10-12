5756f6d036564ad8f28beb997ac4cd4c7e804595
Michael Schramm <michael.schramm@agnet.tamu.edu>
2023-10-12 16:42:47 -0400

    add mission aransas nerr data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,8 +16,33 @@ Dissolved_Oxygen <- readr::read_delim(file = "https://raw.githubusercontent.com/
 usethis::use_data(Dissolved_Oxygen, overwrite = TRUE)
 
 
+## code to prepare `easterwood_weather` dataset goes here
 
 easterwood_weather <- readr::read_csv(file = "data-raw/easterwood.csv",
                                       col_types = "cDcnn")
 
 usethis::use_data(easterwood_weather, overwrite = TRUE)
+
+
+## code to prepare `mission_aransas_nerr` dataset goes here
+
+files <- c("data-raw/marabwq2020.csv",
+           "data-raw/marabwq2021.csv",
+           "data-raw/marcewq2020.csv",
+           "data-raw/marcewq2021.csv")
+
+mission_aransas_nerr <- readr::read_csv(file = files,
+                                 col_types = readr::cols_only(
+                                   StationCode = readr::col_factor(),
+                                   DateTimeStamp = readr::col_datetime(format = "%m/%e/%Y %H:%M"),
+                                   Temp = readr::col_number(),
+                                   F_Temp = readr::col_character(),
+                                   SpCond = readr::col_number(),
+                                   F_SpCond = readr::col_character(),
+                                   Sal = readr::col_number(),
+                                   F_Sal = readr::col_character(),
+                                   DO_mgl = readr::col_number(),
+                                   F_DO_mgl = readr::col_character()
+                                 ))
+
+usethis::use_data(mission_aransas_nerr, overwrite = TRUE)
