d7f5b0687c86913cf7a5c032b96c6225fa266825
Michael Schramm <michael.schramm@agnet.tamu.edu>
2023-10-19 09:43:51 -0400

    skip tests on specific platforms

--- tests/testthat/test-install_font.R
+++ tests/testthat/test-install_font.R
@@ -1,4 +1,11 @@
 test_that("install_font_works", {
   testthat::expect_error(install_font("CrimsonPros"))
+  ## platform dependent results
+  ## on windows returns invisible null
+  ## on macos and linux shold return 0
+  testthat::skip_on_os(c("mac", "linux", "solaris"))
   testthat::expect_null(install_font("CrimsonPro"))
+
+  testthat::skip_on_os(c("windows", "solaris"))
+  testthat::expect_equal(install_font("CrimsonPro"), 0)
 })
